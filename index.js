ca048250a7a154b4d8f03547ec6970a8c0dd68b3
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2019-08-07 18:25:41 -0500

    setting up endpoints

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // implement your API here
+//libraries
+const express = require('express');
+
+//other files
+const db = require('./data/db.js')
+
+//global objects
+const server = express();
+
+//middleware IMPORTANT
+server.use(express.json());
+
+
+server.post('/api/users', (req, res) =>{
+    const newHub = req.body;
+    db.insert(newHub)
+    .then(hub => {
+        res.status(201).json(hub);
+    })
+    .catch(err =>{
+        res.status(500).json({
+            err: err,
+            message: 'failed to create dat hub'
+        })
+    })
+});
+
+
+
+
+
+
+
+
+server.get('/api/users', (req, res) =>{
+    //what is the datatype? express takes care of this
+    //what is my status code? express sends back 200 in this case
+    //what am I sending back? next line
+    res.send('Hello Worldz');
+    });
+
+    server.get('/now', (req, res) =>{
+        res.send(new Date());
+        }); 
